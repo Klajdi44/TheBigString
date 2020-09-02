f6536d6a4973edfacaa3a0f5508becf46379bf97
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2020-09-02 11:18:01 +0200

    added capitalize after - (semiworks)

--- script.js
+++ script.js
@@ -32,12 +32,14 @@ function selectOptions() {
       const firstSpace = name.indexOf(' ');
       const splited = name.slice(0, firstSpace);
       output.value = splited.length;
-    } else if (selected === 'middleName') {
+    }
+    else if (selected === 'middleName') {
       const firstSpace = name.indexOf(' ');
       const lastSpace = name.lastIndexOf(' ');
       const splited = name.substring(firstSpace, lastSpace);
-      output.value = `your middlename start position is ${firstSpace} and last position is ${lastSpace} the middle name is ${splited}`;
-    } else if (selected === 'pngOrJpg') {
+      output.value = `your middlename start position is ${firstSpace + 1} and last position is ${lastSpace - 1} the middle name is ${splited}`;
+    }
+    else if (selected === 'pngOrJpg') {
       if (name.includes('.png')) {
         output.value = `the file is png`;
       } else if (name.includes('.jpg')) {
@@ -46,25 +48,29 @@ function selectOptions() {
       } else {
         output.value = `stop writting gibberish Peter`;
       }
-
-    } else if (selected === 'password') {
-      const replaced = name.replaceAll(length, '*');
+    }
+    else if (selected === 'password') {
+      const replaced = name.replaceAll(name, '*');
       output.value = replaced;
-    } else if (selected === 'thierdCharUppercase') {
+
+    }
+    else if (selected === 'thierdCharUppercase') {
       const begginingOfName = name.substring(0, 2);
       const thierdLetter = name.substring(2, 3);
       const restOfName = name.substring(3);
 
       const glued = `${begginingOfName}${thierdLetter.toUpperCase()}${restOfName}`;
       output.value = glued;
-    } else if (selected === 'hyphen') {
+    }
+    else if (selected === 'hyphen') {
       if (/-/.test(name)) {
-        const hyphenIndex = name.indexOf('-');
-        const beggining = name.substring(0, hyphenIndex);
+        const hyphenIndex = name.indexOf('-'); //returns the index of -
+        const beggining = name.substring(0, hyphenIndex); //returns everything before the -
+        const letterAfter = name.substring(hyphenIndex, hyphenIndex + 2);//returns a
+        const restOf = name.substring(hyphenIndex + 2); //returns the rest of the name after a
 
-
-        console.log(letterAfter);
-
+        const glued = `${beggining}${letterAfter.toUpperCase()}${restOf}`
+        console.log(glued);
       }
 
 
